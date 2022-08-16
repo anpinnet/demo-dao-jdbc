59e2a192c21bb3416ec875db558c86469b055a71
anpinnet <anpinnet@gmail.com>
2022-08-15 21:37:36 -0300

    Classe entidade departamento

---
+++ src/aplicativo/Programa.java
@@ -0,0 +1,15 @@
+package aplicativo;
+
+import entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1, "Livro");
+		
+		System.out.println(obj);
+
+	}
+
+}
