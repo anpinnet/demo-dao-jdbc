568d303dd7a1bc11beb992f4053af98becb1908f
anpinnet <anpinnet@gmail.com>
2022-08-17 20:23:24 -0300

    Interfece DepartamentoDao e VendedorDao

---
+++ src/dao/VendedorDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.util.List;
+import entidades.Vendedor;
+
+public interface VendedorDao {
+	
+	void insert(Vendedor obj);
+	void update(Vendedor obj);
+	void deleteById(Integer id);
+	Vendedor findById(Integer id);
+	List<Vendedor> findAll();
+
+}
